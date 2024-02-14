a01c1d7a925d8c6d6c78dafddce4cdaa9b6e7593
42arch <rend42@163.com>
2024-02-15 00:50:19 +0800

    update

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -1,13 +1,33 @@
 'use client'
 
-import { OrbitControls } from '@react-three/drei'
+import { OrbitControls, Html } from '@react-three/drei'
 import Model from './Model'
+import { useControls } from 'leva'
+import { CITY_OPTIONS } from '@/lib/constants'
+import { Suspense } from 'react'
 
 const Experience = () => {
+  const { city, plane } = useControls({
+    city: {
+      label: '城市',
+      options: CITY_OPTIONS
+    },
+    plane: {
+      label: '平面',
+      value: false
+    }
+  })
+
   return (
     <>
       <OrbitControls makeDefault />
-      <Model />
+      <Suspense fallback={<Html>loading</Html>}>
+        <Model city={city} />
+      </Suspense>
+      <mesh rotation-x={-Math.PI * 0.5} scale={10} visible={plane}>
+        <planeGeometry />
+        <meshBasicMaterial wireframe />
+      </mesh>
     </>
   )
 }
--- src/components/Model.tsx
+++ src/components/Model.tsx
@@ -1,10 +1,18 @@
+import { CITIES } from '@/lib/constants'
 import { useGLTF } from '@react-three/drei'
-import React from 'react'
+interface Props {
+  city: string
+}
 
-const Model = () => {
-  const model = useGLTF('./models/xian.glb')
+const Model = ({ city }: Props) => {
+  const asset = CITIES.find((c) => c.value === city)?.asset
+  const model = useGLTF(asset!)
 
   return <primitive object={model.scene} />
 }
 
 export default Model
+
+CITIES.forEach((city) => {
+  useGLTF.preload(city.asset)
+})
